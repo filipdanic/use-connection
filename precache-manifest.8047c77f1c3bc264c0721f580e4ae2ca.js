03ecbfe290b71022184336491aae922d2c00b0bd
Filip Danić <filip@spicefactory.co>
2019-05-22 12:22:49 +0200

    Updates

---
+++ precache-manifest.8047c77f1c3bc264c0721f580e4ae2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d1e42ea1f6f5f63c58e0034b3bbd18",
+    "url": "/use-connection/index.html"
+  },
+  {
+    "revision": "676748a1645933a462f4",
+    "url": "/use-connection/static/css/main.8b9b1f47.chunk.css"
+  },
+  {
+    "revision": "0e43b4a0914c06829416",
+    "url": "/use-connection/static/js/2.4f4360b6.chunk.js"
+  },
+  {
+    "revision": "676748a1645933a462f4",
+    "url": "/use-connection/static/js/main.224e82b0.chunk.js"
+  },
+  {
+    "revision": "e62b7560d89197237df7",
+    "url": "/use-connection/static/js/runtime~main.ce484b5c.js"
+  },
+  {
+    "revision": "ba328b7d9c5c4a67c7e1de8d2adc3732",
+    "url": "/use-connection/static/media/logo.ba328b7d.png"
+  }
+]);
